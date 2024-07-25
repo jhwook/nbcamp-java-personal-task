7a7db9b4953a2c009d9c1c357a57cad5c2c3d09c
jhwook <jhwooook@naver.com>
2024-07-25 11:09:16 +0900

    Level1_4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,11 +1,17 @@
 package src.main.java.calculator;
 
+import java.util.LinkedList;
 import java.util.Objects;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        Queue<Integer> resultQueue = new LinkedList<>();
+
+        int index = 0;
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -18,23 +24,49 @@ public class App {
 
             int result = 0;
 
+
             switch (operator) {
                 case '+':
                     result = num1 + num2;
+                    index ++;
+                    System.out.println(index);
+                    if(index <= 10) resultQueue.add(result);
+                    else {
+                        resultQueue.poll();
+                        resultQueue.add(result);
+                    }
                     System.out.println("결과 : " + num1 + operator + num2 + " = " + result);
                     break;
                 case '-':
                     result = num1 - num2;
+                    index ++;
+                    if(index <= 10) resultQueue.add(result);
+                    else {
+                        resultQueue.poll();
+                        resultQueue.add(result);
+                    }
                     System.out.println("결과 : " + num1 + operator + num2 + " = " + result);
                     break;
                 case '*':
                     result = num1 * num2;
+                    index ++;
+                    if(index <= 10) resultQueue.add(result);
+                    else {
+                        resultQueue.poll();
+                        resultQueue.add(result);
+                    }
                     System.out.println(STR."결과 : \{num1}\{operator}\{num2} = \{result}");
                     break;
                 case '/':
                     if (num2 == 0) System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     else {
                         result = num1 / num2;
+                        index ++;
+                        if(index <= 10) resultQueue.add(result);
+                        else {
+                            resultQueue.poll();
+                            resultQueue.add(result);
+                        }
                         System.out.println("결과 : " + num1 + operator + num2 + " = " + result);
                     }
                     break;
@@ -45,6 +77,7 @@ public class App {
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String str = sc.next();
             if(Objects.equals(str, "exit")) break;
+            else System.out.println(resultQueue);
         }
     }
 }
